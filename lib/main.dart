e634a7c625e6975c34dcf61a9b6fa54fd9bd0813
admin <darshansakhat166@gmail.com>
2023-02-06 15:13:14 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,510 @@
+//project 1
+
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         body: Center(
+//           child: Container(
+//             width: 300,
+//             height: 300,
+//             color: Colors.red,
+//             child: Center(
+//               child: Container(
+//                 width: 200,
+//                 height: 200,
+//                 color: Colors.yellow,
+//                 child: Center(
+//                   child: Text(
+//                     "Hello",
+//                     style: TextStyle(
+//                         fontSize: 30,
+//                         color: Colors.red,
+//                         fontWeight: FontWeight.bold),
+//                   ),
+//                 ),
+//               ),
+//             ),
+//           ),
+//         ),
+//       ),
+//     ),
+//   );
+// }
+
+// project 2
+
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         body: Center(
+//           child: Container(
+//             width: 300,
+//             height: 300,
+//             color: Colors.red,
+//             alignment: Alignment.bottomLeft,
+//             child: Container(
+//               width: 250,
+//               height: 250,
+//               color: Colors.yellow,
+//               alignment: Alignment.bottomLeft,
+//               child: Container(
+//                 width: 200,
+//                 height: 200,
+//                 color: Colors.lightBlue,
+//                 alignment: Alignment.bottomLeft,
+//                 child: Container(
+//                   width: 150,
+//                   height: 150,
+//                   color: Colors.deepPurpleAccent,
+//                   alignment: Alignment.bottomLeft,
+//                   child: Container(
+//                     width: 100,
+//                     height: 100,
+//                     color: Colors.pink,
+//                     alignment: Alignment.bottomLeft,
+//                     child: Container(
+//                       width: 50,
+//                       height: 50,
+//                       color: Colors.red,
+//                       child: Center(
+//                         child: Text(
+//                           "Hello",
+//                           style: TextStyle(
+//                               color: Colors.white, fontWeight: FontWeight.bold),
+//                         ),
+//                       ),
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//             ),
+//           ),
+//         ),
+//       ),
+//     ),
+//   );
+// }
+
+// project 3
+
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         body: Center(
+//           child: Container(
+//             width: 300,
+//             height: 300,
+//             color: Colors.red,
+//             alignment: Alignment.centerLeft,
+//             child: Container(
+//               width: 250,
+//               height: 300,
+//               color: Colors.yellow,
+//               alignment: Alignment.topCenter,
+//               child: Container(
+//                 width: 250,
+//                 height: 150,
+//                 color: Colors.blue,
+//                 alignment: Alignment.topLeft,
+//                 child: Container(
+//                   width: 125,
+//                   height: 150,
+//                   color: Colors.purple,
+//                   alignment: Alignment.topCenter,
+//                   child: Container(
+//                     width: 125,
+//                     height: 75,
+//                     color: Colors.pink,
+//                     alignment: Alignment.topLeft,
+//                     child: Container(
+//                       width: 62.5,
+//                       height: 75,
+//                       color: Colors.orange,
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//             ),
+//           ),
+//         ),
+//       ),
+//     ),
+//   );
+// }
+
+// project 4
+
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         body: Center(
+//           child: Container(
+//             width: 300,
+//             height: 300,
+//             color: Colors.red,
+//             child: Container(
+//               width: 300,
+//               height: 300,
+//               decoration: BoxDecoration(
+//                 borderRadius: BorderRadius.only(
+//                     topLeft: Radius.circular(500),
+//                     bottomRight: Radius.circular(500)),
+//                 color: Colors.yellow,
+//               ),
+//               alignment: Alignment.bottomLeft,
+//               child: Container(
+//                 width: 200,
+//                 height: 200,
+//                 decoration: BoxDecoration(
+//                   borderRadius: BorderRadius.only(
+//                       topLeft: Radius.circular(500),
+//                       bottomRight: Radius.circular(500)),
+//                   color: Colors.blue,
+//                 ),
+//                 alignment: Alignment.bottomLeft,
+//                 child: Container(
+//                   width: 100,
+//                   height: 100,
+//                   decoration: BoxDecoration(
+//                     borderRadius: BorderRadius.only(
+//                         topLeft: Radius.circular(500),
+//                         bottomRight: Radius.circular(500),
+//                         topRight: Radius.circular(100)),
+//                     color: Colors.purple,
+//                   ),
+//                 ),
+//               ),
+//             ),
+//           ),
+//         ),
+//       ),
+//     ),
+//   );
+// }
+
+// project 5
+
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         body: Center(
+//           child: Container(
+//             width: 300,
+//             height: 300,
+//             color: Colors.red,
+//             alignment: Alignment.center,
+//             child: Container(
+//               width: 250,
+//               height: 250,
+//               color: Colors.yellow,
+//               alignment: Alignment.center,
+//               child: Container(
+//                 width: 250,
+//                 height: 250,
+//                 decoration: BoxDecoration(
+//                   shape: BoxShape.circle,
+//                   color: Colors.blue,
+//                 ),
+//                 alignment: Alignment.center,
+//                 child: Container(
+//                   width: 175,
+//                   height: 175,
+//                   color: Colors.purple,
+//                   alignment: Alignment.center,
+//                   child: Container(
+//                     width: 175,
+//                     height: 175,
+//                     decoration: BoxDecoration(
+//                         borderRadius: BorderRadius.only(
+//                             topLeft: Radius.circular(500),
+//                             bottomRight: Radius.circular(500)),
+//                         color: Colors.pink),
+//                     alignment: Alignment.center,
+//                     child: Text(
+//                       "Hello",
+//                       style: TextStyle(
+//                           color: Colors.black,
+//                           fontWeight: FontWeight.bold,
+//                           fontSize: 30),
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//             ),
+//           ),
+//         ),
+//       ),
+//     ),
+//   );
+// }
+
+// project 6
+
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         body: Center(
+//           child: Container(
+//             width: 300,
+//             height: 300,
+//             color: Colors.red,
+//             alignment: Alignment.topLeft,
+//             child: Container(
+//               width: 250,
+//               height: 250,
+//               decoration: BoxDecoration(
+//                   borderRadius: BorderRadius.only(
+//                       topRight: Radius.circular(500),
+//                       bottomRight: Radius.circular(500),
+//                       bottomLeft: Radius.circular(500)),
+//                   color: Colors.yellow),
+//               alignment: Alignment.topLeft,
+//               child: Container(
+//                 width: 200,
+//                 height: 200,
+//                 decoration: BoxDecoration(
+//                     borderRadius: BorderRadius.only(
+//                         topRight: Radius.circular(500),
+//                         bottomRight: Radius.circular(500),
+//                         bottomLeft: Radius.circular(500)),
+//                     color: Colors.purple),
+//               ),
+//             ),
+//           ),
+//         ),
+//       ),
+//     ),
+//   );
+// }
+
+// project 7
+
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         body: Center(
+//           child: Container(
+//             width: 350,
+//             height: 350,
+//             color: Colors.red,
+//             alignment: Alignment.topLeft,
+//             child: Container(
+//               width: 300,
+//               height: 300,
+//               color: Colors.yellow,
+//               alignment: Alignment.bottomRight,
+//               child: Container(
+//                 width: 250,
+//                 height: 250,
+//                 color: Colors.purple,
+//                 alignment: Alignment.topLeft,
+//                 child: Container(
+//                   width: 200,
+//                   height: 200,
+//                   color: Colors.blue,
+//                   alignment: Alignment.bottomRight,
+//                   child: Container(
+//                     width: 150,
+//                     height: 150,
+//                     color: Colors.amber,
+//                     alignment: Alignment.topLeft,
+//                     child: Container(
+//                       width: 100,
+//                       height: 100,
+//                       color: Colors.pink,
+//                       alignment: Alignment.topRight,
+//                       child: Container(
+//                         width: 50,
+//                         height: 50,
+//                         color: Colors.redAccent,
+//                         alignment: Alignment.center,
+//                         child: Text(
+//                           "D",
+//                           style: TextStyle(
+//                               color: Colors.black,
+//                               fontWeight: FontWeight.bold,
+//                               fontSize: 40),
+//                         ),
+//                       ),
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//             ),
+//           ),
+//         ),
+//       ),
+//     ),
+//   );
+// }
+
+// project 8
+
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         body: Center(
+//           child: Container(
+//             width: 500,
+//             height: 500,
+//             color: Colors.red,
+//             alignment: Alignment.topLeft,
+//             child: Container(
+//               width: 400,
+//               height: 300,
+//               color: Colors.yellow,
+//               alignment: Alignment.bottomCenter,
+//               child: Container(
+//                 width: 400,
+//                 height: 200,
+//                 color: Colors.blue,
+//                 alignment: Alignment.centerRight,
+//                 child: Container(
+//                   width: 200,
+//                   height: 200,
+//                   color: Colors.purple,
+//                   alignment: Alignment.bottomCenter,
+//                   child: Container(
+//                     width: 200,
+//                     height: 100,
+//                     color: Colors.pink,
+//                     alignment: Alignment.centerRight,
+//                     child: Container(
+//                       width: 100,
+//                       height: 100,
+//                       color: Colors.amber,
+//                       alignment: Alignment.bottomCenter,
+//                       child: Container(
+//                         width: 100,
+//                         height: 50,
+//                         color: Colors.lightBlueAccent,
+//                         alignment: Alignment.centerRight,
+//                         child: Container(
+//                           width: 50,
+//                           height: 50,
+//                           color: Colors.green,
+//                           alignment: Alignment.bottomCenter,
+//                           child: Container(
+//                             width: 50,
+//                             height: 25,
+//                             color: Colors.teal,
+//                             alignment: Alignment.centerRight,
+//                             child: Container(
+//                               width: 25,
+//                               height: 25,
+//                               color: Colors.white,
+//                             ),
+//                           ),
+//                         ),
+//                       ),
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//             ),
+//           ),
+//         ),
+//       ),
+//     ),
+//   );
+// }
+
+// project 9
+
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            width: 300,
+            height: 300,
+            color: Colors.red,
+            alignment: Alignment.center,
+            child: Container(
+              width: 300,
+              height: 300,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.yellow,
+              ),
+              alignment: Alignment.center,
+              child: Transform.rotate(
+                angle: pi / 4,
+                child: Container(
+                  width: 210,
+                  height: 210,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(500),
+                          bottomRight: Radius.circular(500)),
+                      color: Colors.pink),
+                  alignment: Alignment.center,
+                  child: Transform.rotate(
+                    angle: pi / 2,
+                    child: Container(
+                      width: 90,
+                      height: 90,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(500),
+                            bottomRight: Radius.circular(500),
+                          ),
+                          color: Colors.blue),
+                      alignment: Alignment.center,
+                      child: Container(
+                        width: 55,
+                        height: 55,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.amber,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
